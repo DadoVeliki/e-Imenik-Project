7c2496023b8d37e0bfca7c6ac217e5afb45177f2
David Trstenjak <96396254+DadoVeliki@users.noreply.github.com>
2022-06-07 22:41:38 +0200

    Update MainActivity.java

--- java/com/example/projekt_david_trstenjak/MainActivity.java
+++ java/com/example/projekt_david_trstenjak/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.EmptyStackException;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,21 +26,33 @@ public class MainActivity extends AppCompatActivity {
         EditText lozinka=(EditText) findViewById(R.id.password);
         String user=ime.getText().toString();
         String pass=lozinka.getText().toString();
+        String imeIprezime="";
 
-        if(user.contains("@skole.hr") && pass.equals("123")) {
-            String name = "David Trstenjak";
-            i.putExtra("ime", name);
-            startActivity(i);
-            napomena.setText(" ");
-        }
-        else if(user.contains("@skole.hr") && pass.equals("456")) {
-            String name = "Lukas Kraljić";
-            i.putExtra("ime", name);
-            startActivity(i);
-            napomena.setText(" ");
+        imeIprezime+=user.substring(0,1).toUpperCase();
+        imeIprezime+=(user.substring(1,user.indexOf('.')));
+        imeIprezime+=" ";
+        imeIprezime+=(user.substring((user.indexOf('.'))+1,(user.indexOf('.'))+2)).toUpperCase();
+        imeIprezime+=user.substring(user.indexOf('.')+2,user.indexOf('@'));
+try{
+
+
+        if(user.contains("@skole.hr")) {
+            i.putExtra("ime", imeIprezime);
+            napomena.setText("");
+            try{
+                Integer.parseInt(pass);
+                startActivity(i);
+            }
+            catch (NumberFormatException e){
+                napomena.append("Neispravna lozinka.");
+            }
         }
         else{
-            napomena.setText("Neispravno korisničko ime ili lozinka.");
+            napomena.setText("Neispravno korisničko ime.");
         }
+}
+catch(EmptyStackException e){
+    napomena.setText("Neispravno korisničko ime ili lozinka.");
+}
     }
 }
